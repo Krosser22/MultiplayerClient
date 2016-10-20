2c8fc95ff0c9f68f72e8e22e30c4f0d797942725
DESKTOP-A69K1CU\conda <condadodaniel@hotmail.com>
2016-10-20 16:25:10 +0100

    Added libraries and updated genie.

--- genie/genie.lua
+++ genie/genie.lua
@@ -16,19 +16,36 @@ solution "multiplayer"
   location (_OPTIONS["to"])
   
   configuration "Debug"
-    defines     { "_DEBUG", "LUA_COMPAT_MODULE" }
-    flags       { "Symbols" }
+    defines { "_DEBUG", "LUA_COMPAT_MODULE" }
+    flags { "Symbols" }
     debugdir "../project/build/bin/debug"
     os.mkdir "../project/build/bin/debug"
+    links {
+      "openal32",
+      "Box2D",
+      "sfml-audio-d",
+      "sfml-graphics-d",
+      "sfml-network-d",
+      "sfml-system-d",
+      "sfml-window-d",
+    }
 
   configuration "Release"
-    defines     { "NDEBUG", "LUA_COMPAT_MODULE" }
-    flags       { "Optimize" }
+    defines { "NDEBUG", "LUA_COMPAT_MODULE" }
+    flags { "Optimize" }
     debugdir "../project/build/bin/release"
     os.mkdir "../project/build/bin/release"
+    links {
+      "openal32",
+      "Box2D",
+      "sfml-graphics",
+      "sfml-network",
+      "sfml-system",
+      "sfml-window",
+    }
 
   configuration "vs*"
-    defines     { "_CRT_SECURE_NO_WARNINGS" }
+    defines { "_CRT_SECURE_NO_WARNINGS" }
 
   configuration "windows"
     targetdir "../project/build/bin/windows"
@@ -38,15 +55,36 @@ solution "multiplayer"
 -- this to create project files for each supported toolset, each in their own folder,
 -- in preparation for deployment.
 --
-	newoption {
-		trigger = "to",
-		value   = "path",
-		description = "Set the output location for the generated files"
-	}
+  newoption {
+    trigger = "to",
+    value   = "path",
+    description = "Set the output location for the generated files"
+  }
   
   os.mkdir("../project/build/bin/windows")
-  --os.copyfile("../lib/openal32.dll", "../project/build/bin/windows/openal32.dll")
-  --os.copyfile("../lib/openal32.lib", "../project/build/bin/windows/openal32.lib")
+  os.copyfile("../lib/openal32.lib", "../project/build/bin/windows/openal32.lib")
+  os.copyfile("../lib/openal32.dll", "../project/build/bin/windows/openal32.dll")
+  os.copyfile("../lib/Box2D.lib", "../project/build/bin/windows/Box2D.lib")
+  os.copyfile("../lib/sfml-audio-2.dll", "../project/build/bin/windows/sfml-audio-2.dll")
+  os.copyfile("../lib/sfml-audio.lib", "../project/build/bin/windows/sfml-audio.lib")
+  os.copyfile("../lib/sfml-audio-d-2.dll", "../project/build/bin/windows/sfml-audio-d-2.dll")
+  os.copyfile("../lib/sfml-audio-d.lib", "../project/build/bin/windows/sfml-audio-d.lib")
+  os.copyfile("../lib/sfml-graphics-2.dll", "../project/build/bin/windows/sfml-graphics-2.dll")
+  os.copyfile("../lib/sfml-graphics.lib", "../project/build/bin/windows/sfml-graphics.lib")
+  os.copyfile("../lib/sfml-graphics-d-2.dll", "../project/build/bin/windows/sfml-graphics-d-2.dll")
+  os.copyfile("../lib/sfml-graphics-d.lib", "../project/build/bin/windows/sfml-graphics-d.lib")
+  os.copyfile("../lib/sfml-network-2.dll", "../project/build/bin/windows/sfml-network-2.dll")
+  os.copyfile("../lib/sfml-network.lib", "../project/build/bin/windows/sfml-network.lib")
+  os.copyfile("../lib/sfml-network-d-2.dll", "../project/build/bin/windows/sfml-network-d-2.dll")
+  os.copyfile("../lib/sfml-network-d.lib", "../project/build/bin/windows/sfml-network-d.lib")
+  os.copyfile("../lib/sfml-system-2.dll", "../project/build/bin/windows/sfml-system-2.dll")
+  os.copyfile("../lib/sfml-system.lib", "../project/build/bin/windows/sfml-system.lib")
+  os.copyfile("../lib/sfml-system-d-2.dll", "../project/build/bin/windows/sfml-system-d-2.dll")
+  os.copyfile("../lib/sfml-system-d.lib", "../project/build/bin/windows/sfml-system-d.lib")
+  os.copyfile("../lib/sfml-window-2.dll", "../project/build/bin/windows/sfml-window-2.dll")
+  os.copyfile("../lib/sfml-window.lib", "../project/build/bin/windows/sfml-window.lib")
+  os.copyfile("../lib/sfml-window-d-2.dll", "../project/build/bin/windows/sfml-window-d-2.dll")
+  os.copyfile("../lib/sfml-window-d.lib", "../project/build/bin/windows/sfml-window-d.lib")
 
 --[[--------------------------------------------
 ------------- MULTIPLAYER PROJECT --------------
@@ -57,26 +95,23 @@ project "multiplayer"
   location "../project/build"
   libdirs "../lib"
   kind "ConsoleApp"
-  --kind "StaticLib"
   
   --[[flags {
     "No64BitChecks",
     "ExtraWarnings",
   }]]
-  
-  links {
-    --"opengl32",
-    --"openal32",
-  }
 
-  defines {
+  --defines {
     --"_GLFW_WIN32",
     --"_GLFW_WGL",
     --"_GLFW_USE_OPENGL",
-  }
+  --}
   
   includedirs {
-    --"../include",
+    "../assets",
+    "../include",
+    "../deps/Box2D",
+    "../deps/SFML/include",
     --"../examples/include",
     --"../deps/glew/include",
     --"../deps/glfw/include",
@@ -89,8 +124,10 @@ project "multiplayer"
   }
   
   files {
-    --"../include/**.h",
-    --"../src/**.*",
+    "../src/**.*",
+    "../include/**.*",
+    "../deps/SFML/include/**.*",
+    "../deps/Box2D/Box2D/Box2D.h",
     --"../deps/glfw/src/*.c",
     --"../deps/imgui/*.cpp",
     --"../deps/soil/*.c",
--- genie/genie_start.bat
+++ genie/genie_start.bat
@@ -1,10 +1,10 @@
 ::
 :: ////////////////////////////////////////////
 :: /////Autor: Juan Daniel Laserna Condado/////
-:: /////Email: lasernaco@esat-alumni.com  /////
-:: /////            2015-2016             /////
+:: /////Email: S6106112@live.tees.ac.uk   /////
+:: /////            2016-2017             /////
 :: ////////////////////////////////////////////
 ::
 
-genie.exe --dotnet=msnet --file=genie.lua --os=windows --platform=x64 --to=../project vs2013
+genie.exe --dotnet=msnet --file=genie.lua --os=windows --platform=x64 --to=../project vs2015
 pause
